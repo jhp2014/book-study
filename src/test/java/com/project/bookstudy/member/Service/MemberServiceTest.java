3c5d109ce07a408751b8eebab40fded78fef5efb
Son of Jongkyun, Park <100128652+jhp2014@users.noreply.github.com>
2023-08-21 21:19:58 +0900

    feat : Test Service & Controller
    1. 기본적인 회원 가입 테스트 완료
    2. 회원가입 입력 조건에 맞지 않는 경우 에러 테스트 완료
    3. 중복된 이메일이 존재하는 경우 예외 발생 테스트 완료

---
+++ src/test/java/com/project/bookstudy/member/Service/MemberServiceTest.java
@@ -0,0 +1,94 @@
+package com.project.bookstudy.member.Service;
+
+import com.project.bookstudy.member.domain.Member;
+import com.project.bookstudy.member.domain.Role;
+import com.project.bookstudy.member.dto.CreateMemberRequest;
+import com.project.bookstudy.member.dto.MemberDto;
+import com.project.bookstudy.member.exception.DuplicateEmail;
+import com.project.bookstudy.member.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired
+    private MemberService memberService;
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("회원가입 성공 테스트")
+    void successJoinMember() {
+        //given
+        String name = "name";
+        String password = "!@!@fjaweio31";
+        String phone = "010-7474-8181";
+        String email = "whdgnsdl35@naver.com";
+        String career = "취준생";
+
+        CreateMemberRequest request = CreateMemberRequest.builder()
+            .name(name)
+            .password(password)
+            .phone(phone)
+            .email(email)
+            .career(career)
+            .build();
+
+        //when
+        MemberDto memberDto = memberService.joinMember(request);
+
+        //then
+        Member findMember = memberRepository.findById(memberDto.getId()).get();
+
+        Assertions.assertThat(findMember.getName()).isEqualTo(request.getName());
+        Assertions.assertThat(findMember.getEmail()).isEqualTo(request.getEmail());
+        Assertions.assertThat(findMember.getPassword()).isEqualTo(request.getPassword());
+        Assertions.assertThat(findMember.getCareer()).isEqualTo(request.getCareer());
+        Assertions.assertThat(findMember.getPhone()).isEqualTo(request.getPhone());
+        Assertions.assertThat(findMember.getPoint()).isEqualTo(0L);
+        Assertions.assertThat(findMember.getRole()).isEqualTo(Role.MEMBER);
+        Assertions.assertThat(findMember.getRegisteredAt()).isNotNull();
+        Assertions.assertThat(findMember.getUpdatedAt()).isNotNull();
+        Assertions.assertThat(findMember.getIsActivated()).isTrue();
+    }
+
+    @Test
+    @DisplayName("이메일 중복으로 인한 회원가입 실패 ")
+    @Transactional
+    void failJoinMember() {
+        //given
+        String name = "name";
+        String password = "!@fdsajkwh2321";
+        String phone = "010-7474-8181";
+        String email = "whdgnsdl35@naver.com";
+        String career = "취준생";
+
+        Member member = Member.builder()
+                .email(email)
+                .password(password)
+                .phone(phone)
+                .name(name)
+                .career(career)
+                .build();
+
+        memberRepository.save(member);
+
+        CreateMemberRequest request = CreateMemberRequest.builder()
+                .name(name)
+                .password(password)
+                .phone(phone)
+                .email(email)
+                .career(career)
+                .build();
+
+        //expected
+        Assertions.assertThatThrownBy(() -> memberService.joinMember(request))
+                .isInstanceOf(DuplicateEmail.class);
+    }
+}
